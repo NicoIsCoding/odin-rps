edac7d9f59f372d1cfa1c264426aa87435bffc02
NicoIsCoding <nicoswift22@gmail.com>
2025-07-14 22:37:52 +0200

    Remove comments

--- javascript.js
+++ javascript.js
@@ -1,10 +1,4 @@
-//Pseudo code
-//Generate computer choice "rock", "paper", "scissors"
-//Prompt player choice
-//Convert player choice to lowercase
-//Create function that determines win/lose combos
-//Create a loop that replays the game 5 times -> gameRound variable
-//Check points at the end and declare winner
+
 
 let computerOptions = ["rock", "paper", "scissors"];
 let computerChoice;
