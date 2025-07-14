f64d53973cf91ddf3764536307bb131ba247b344
NicoIsCoding <nicoswift22@gmail.com>
2025-07-14 20:14:29 +0200

    Add game logic, false input system and final score
    Finished the entire game logic, false input system, point tracker and a final point display
    declaring the winner.

--- javascript.js
+++ javascript.js
@@ -6,18 +6,93 @@
 //Create a loop that replays the game 5 times -> gameRound variable
 //Check points at the end and declare winner
 
-let computerChoice = ["rock", "paper", "scissors"];
+let computerOptions = ["rock", "paper", "scissors"];
 let playerChoice;
-let computerPoints;
-let playerPoint;
-let gameRound = 1;
+let computerPoints = 0;
+let playerPoints = 0;
+let gameRound = 0;
 
 function getComputerChoice(choice){
     let index =Math.floor(Math.random() * choice.length)
     return choice[index];
 }
 
-console.log(getComputerChoice(computerChoice))
+function checkInput(player){
+    if (player !== "rock" && player !== "paper" && player !== "scissors"){
+        return console.log("your input is invalid!") 
+    } else {
+        return true;
+    }
+        
+}
+
+function gameLogic(player, cpu) {
+    if (player == "rock" && cpu == "paper") {
+        alert("computer picks paper and BEATS your rock!")
+        computerPoints++;
+        gameRound++;
+        alert(`Round: ${gameRound} Cpu: ${computerPoints} points Player: ${playerPoints} points`)
+
+    } else if (player == "paper" && cpu == "scissors") {
+        alert("computer picks scissors and BEATS your paper!")
+        computerPoints++;
+        gameRound++;
+        alert(`Round: ${gameRound} Cpu: ${computerPoints} points Player: ${playerPoints} points`) 
+
+    } else if (player == "scissors" && cpu == "rock") {
+        alert("computer picks rock and BEATS your scissors!")
+        computerPoints++;
+        gameRound++;
+        alert(`Round: ${gameRound} Cpu: ${computerPoints} points Player: ${playerPoints} points`)
+
+    } else if (player == "rock" && cpu == "scissors") {
+        alert("computer picks scissors and you SMASH them with your rock!")
+        playerPoints++;
+        gameRound++;
+        alert(`Round: ${gameRound} Cpu: ${computerPoints} points Player: ${playerPoints} points`)
+
+    } else if (player == "paper" && cpu == "rock") {
+        alert("computer picks rock and you SMASH it with your paper!")
+        playerPoints++;
+        gameRound++;
+        alert(`Round: ${gameRound} Cpu: ${computerPoints} points Player: ${playerPoints} points`)
+
+    } else if (player == "scissors" && cpu == "paper"){
+        alert("computer picks paper and you SMASH it with your scissors")
+        playerPoints++;
+        gameRound++;
+        alert(`Round: ${gameRound} Cpu: ${computerPoints} points Player: ${playerPoints} points`)
+
+    } else {
+        alert(`Computer picks: ${cpu.toUpperCase()} Player picks: ${player.toUpperCase()}! It's a DRAW!`)
+        gameRound++;
+        alert(`Round: ${gameRound} Cpu: ${computerPoints} points Player: ${playerPoints} points`)
+    }
+}
+
+while (gameRound < 5) {
+    computerChoice = getComputerChoice(computerOptions);
+    playerChoice = prompt("rock, paper or scissors?").toLowerCase();
+
+    if (checkInput(playerChoice) == true) {
+        gameLogic(playerChoice, computerChoice);
+    } else {
+        alert("your input is INVALID!")
+        continue;
+    }
+
+}
+
+if (playerPoints > computerPoints) {
+    alert(`Player scored ${playerPoints} out of 5 points and WON the bout!`)
+
+} else if (playerPoints < computerPoints) {
+    alert(`Computer scored ${computerPoints} out of 5 points and WON the bout!`)
+    
+} else {
+    alert(`Both players scored ${playerPoints}! It's a DRAW!`)
+}
+
 
 
 
