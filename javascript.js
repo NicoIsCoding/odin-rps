61bbc29684a4d3884320facc1f1262d35c39f314
NicoIsCoding <nicoswift22@gmail.com>
2025-08-22 00:22:37 +0200

    create UI for rock-paper-scissors game

--- javascript.js
+++ javascript.js
@@ -22,40 +22,86 @@ function checkInput(player){
 }
 
 function gameLogic(player, cpu) {
+
+
     if (player == "rock" && cpu == "paper") {
-        alert("computer picks paper and BEATS your rock!")
+        scorePara.textContent = "computer picks paper and BEATS your rock!"
         computerPoints++;
 
     } else if (player == "paper" && cpu == "scissors") {
-        alert("computer picks scissors and BEATS your paper!")
+        scorePara.textContent = "computer picks scissors and BEATS your paper!"
         computerPoints++;
 
     } else if (player == "scissors" && cpu == "rock") {
-        alert("computer picks rock and BEATS your scissors!")
+        scorePara.textContent = "computer picks rock and BEATS your scissors!"
         computerPoints++;
 
     } else if (player == "rock" && cpu == "scissors") {
-        alert("computer picks scissors and you SMASH them with your rock!")
+        scorePara.textContent = "computer picks scissors and you SMASH them with your rock!"
         playerPoints++;
 
     } else if (player == "paper" && cpu == "rock") {
-        alert("computer picks rock and you SMASH it with your paper!")
+        scorePara.textContent = "computer picks rock and you SMASH it with your paper!"
         playerPoints++;
 
     } else if (player == "scissors" && cpu == "paper"){
-        alert("computer picks paper and you SMASH it with your scissors")
+        scorePara.textContent = "computer picks paper and you SMASH it with your scissors"
         playerPoints++;
 
     } else {
-        alert(`Computer picks: ${cpu.toUpperCase()} Player picks: ${player.toUpperCase()}! It's a DRAW!`)
+        scorePara.textContent = `Computer picks: ${cpu} Player picks: ${player}! It's a DRAW!`
 
     }
     
     gameRound++;
-    alert(`Round: ${gameRound} Cpu: ${computerPoints} points Player: ${playerPoints} points`)
+    scoreDisplay.textContent = `Round: ${gameRound} Cpu: ${computerPoints} points Player: ${playerPoints} points`
+
+    if (gameRound === 5) {
+    if (playerPoints > computerPoints) {
+    scorePara.textContent = `Player scored ${playerPoints} out of 5 points and WON the bout!`
+    buttons.forEach((button) => button.remove())
+    scoreDisplay.remove()
+
+    } else if (playerPoints < computerPoints) {
+    scorePara.textContent = `Computer scored ${computerPoints} out of 5 points and WON the bout!`
+    buttons.forEach((button) => button.remove())
+    scoreDisplay.remove()
+
+    } else {
+    scorePara.textContent = `Both players scored ${playerPoints}! It's a DRAW!`
+    buttons.forEach((button) => button.remove())
+    scoreDisplay.remove()
+
+} 
+    let button = document.createElement("button")
+    button.textContent = "RESTART"
+    body.appendChild(button)
+
+    button.addEventListener("click", () => location.reload())
+    
+}
 }
 
-while (gameRound < 5) {
+const rock = document.querySelector("#rock")
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
+const buttons = document.querySelectorAll("button")
+const body = document.querySelector("body")
+let scoreDisplay = document.querySelector("#score")
+scoreDisplay.textContent = "Click to play!"
+let scorePara = document.createElement("div")
+body.appendChild(scorePara)
+
+
+
+rock.addEventListener("click", () => gameLogic("rock", getComputerChoice(computerOptions)))
+paper.addEventListener("click", () => gameLogic("paper", getComputerChoice(computerOptions)))
+scissors.addEventListener("click", () => gameLogic("scissors", getComputerChoice(computerOptions)))
+
+
+
+
+/* while (gameRound < 5) {
     computerChoice = getComputerChoice(computerOptions);
     playerChoice = prompt("rock, paper or scissors?").toLowerCase();
 
@@ -76,13 +122,13 @@ if (playerPoints > computerPoints) {
 
 } else {
     alert(`Both players scored ${playerPoints}! It's a DRAW!`)
-}
+} 
 
-let playAgain = confirm("Do you wanna play again?")
+let playAgain = confirm("Do you wanna play again?") 
 
 playAgain == true 
 ? location.reload() 
-: alert("Thanks for playing!")
+: alert("Thanks for playing!") */
 
 
 
